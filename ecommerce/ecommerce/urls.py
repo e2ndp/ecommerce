861c6ff0b7683de7f756054c22fc15968c7cf65b
NASINE DE PREZ Eduardo Ezequiel <e2ndp@gmail.com>
2022-08-15 12:01:25 -0300

    Inicio arreglado

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from ecommerce.views import homepage, nosotros
+from ecommerce.views import index, nosotros
 #from products.views import create_product, list_products, create_combos, list_combos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', homepage, name='home'),
+    path('', index, name='home'),
     path('nosotros/', nosotros, name='nosotros'),
     path('products/', include('products.urls')),
 ]
--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(request):
-    return render(request, 'index.html', context={})
+def index(request):
+    return render(request, 'index.html')
 
 def nosotros(request):
     return render(request, 'nosotros.html', context={})
